c6bda519ac6c26c248d11c49445fd75b0ad4295d
pugachoveu <pugachov.eu@gmail.com>
2018-06-05 01:26:11 +0300

    add presentation

--- EmoticonAnalysis/PresentationLayer/WebEmoticonAnalysis/Controllers/HomeController.cs
+++ EmoticonAnalysis/PresentationLayer/WebEmoticonAnalysis/Controllers/HomeController.cs
@@ -21,20 +21,6 @@ namespace WebEmoticonAnalysis.Controllers
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
-
         [HttpPost]
         public async System.Threading.Tasks.Task<ActionResult> AnalyzeTweetAsync(TweetAnalyzeViewModel model)
         {
